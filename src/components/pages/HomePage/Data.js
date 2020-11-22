b348d47812ef3937777bc1cfb8e094996e55cbd4
Shuib Abdillahi <shuibabdillahi@MacBook-Pro.local>
2020-11-22 18:45:49 +0000

    First commit

---
+++ src/components/pages/HomePage/Data.js
@@ -0,0 +1,12 @@
+export const homeObjOne = {
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Exclusive Access',
+    headline: 'Unlimited Transactions For Premium Members',
+    description: 'Get access to our huge range of credit and debit cards that allows you to send unlimited transactions without getting charged any fees!',
+    buttonLabel: 'Get Started ',
+    imgStart: '',
+    img: 'images/svg-1.svg',
+    alt: 'Credit Card'
+}
