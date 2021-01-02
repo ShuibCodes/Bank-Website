116ed52fdbfcd5a780a2d387cdba124eccdae55a
Shuib Abdillahi <abdullahshuib124@gmail.com>
2021-01-02 22:58:30 +0000

    fixed view on mobile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState} from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 import { MdFingerprint } from 'react-icons/md';
@@ -7,25 +7,9 @@ import { IconContext } from 'react-icons/lib';
 
 function Navbar() {
   const [click, setClick] = useState(false);
-  const [button, setButton] = useState(true);
 
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
-
-  const showButton = () => {
-    if (window.innerWidth <= 960) {
-      setButton(false);
-    } else {
-      setButton(true);
-    }
-  };
-
-  useEffect(() => {
-    showButton();
-    window.addEventListener('resize', showButton);
-   
-  }, []);
-
 
   return (
     <>
@@ -64,15 +48,13 @@ function Navbar() {
                 </Link>
               </li>
               <li className='nav-btn'>
-                {button ? (
-                  <Link to='/sign-up' className='btn-link'>
-                  Sign Up
-                  </Link>
-                ) : (
-                  <Link to='/sign-up' className='btn-link' >
-                  Sign Up
-                  </Link>
-                )}
+              <Link
+                  to='/sign-up'
+                  className='nav-links'
+                  onClick={closeMobileMenu}
+                >
+                  Sign up
+                </Link>
               </li>
             </ul>
           </div>
